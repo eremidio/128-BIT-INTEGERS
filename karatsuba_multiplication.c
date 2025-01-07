c4bd3cf0a7d31e7856b553d82f0c6b1047839161
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2025-01-07 16:19:07 -0300

    Add files via upload

--- karatsuba_multiplication.c
+++ karatsuba_multiplication.c
@@ -8,36 +8,35 @@
 //Função principal
 int main(){
 
- //Variáveis locais
- char number_string1[40], number_string2[40], number_string3[40];
- __int128_t first, second, base , result;
+  //Variáveis locais
+  char number_string1[40], number_string2[40], number_string3[40];
+  __int128_t first, second, base , result;
 
 
  //Procedimentos
+    //Recebendo input
+    printf("Primeira parcela: ");
+    scanf("%s", number_string1);
 
- //Recebendo input
- printf("Primeira parcela: ");
- scanf("%s", number_string1);
 
+    printf("Segunda parcela: ");
+    scanf("%s", number_string2);
 
- printf("Segunda parcela: ");
- scanf("%s", number_string2);
+    printf("Base de redução: ");
+    scanf("%s", number_string3);
 
- printf("Base de redução: ");
- scanf("%s", number_string3);
+    first=scanf128(number_string1);
+    second=scanf128(number_string2);
+    base=scanf128(number_string3);
 
- first=scanf128(number_string1);
- second=scanf128(number_string2);
- base=scanf128(number_string3);
+    //Calculando o produto das duas parcelas
+    result=karatsuba_multiplication(base, first, second);
 
- //Calculando o produto das duas parcelas
- result=karatsuba_multiplication(base, first, second);
-
- printf("Resultado: ");
- printf128(result);
+    printf("Resultado: ");
+    printf128(result);
 
  
- //Finalizando a aplicação
- return 0;
+  //Finalizando a aplicação
+  return 0;
 
-          }
+}
--- karatsuba_multiplication.h
+++ karatsuba_multiplication.h
@@ -26,19 +26,19 @@ __int128_t karatsuba_multiplication(__int128_t, __int128_t, __int128_t);
 //Função que separa um número de em duas parcelas contendo seu digitos mais e menos signficativos
 void split_number(__int128_t* low, __int128_t* high, __int128_t number, __int128_t B){
 
- //Resultado
- (*low)=(number%B);
- (*high)=(number/B);
-                                                                                     };
+  //Resultado
+  (*low)=(number%B);
+  (*high)=(number/B);
+
+};
 
 //Função que implementa o algoritmo de Karatsuba para multiplicar dois inteiros
 __int128_t karatsuba_multiplication(__int128_t base, __int128_t first, __int128_t second){
 
- //Caso trivial: as parcelas são menores que o elemento usado para redução da string númerica 
- if(first<=base || second<=base)
-  return second*first;
+  //Caso trivial: as parcelas são menores que o elemento usado para redução da string númerica 
+  if(first<=base || second<=base) return second*first;
 
- //Caso: aplicação recursiva do algoritmo de redução de Karatsuba
+    //Caso geral: aplicação recursiva do algoritmo de redução de Karatsuba
 
   //Variáveis locais
   __int128_t low1, high1, low2, high2;
@@ -49,38 +49,31 @@ __int128_t karatsuba_multiplication(__int128_t base, __int128_t first, __int128_
 
 
   //Procedimentos
+    //Estimando a ordem de grandeza do fator de redução
+    while(B<first && B<second){
+      m++;
+      B*=base;
+    };
 
-  //Estimando a ordem de grandeza do fator de redução
-  while(B<first && B<second){
-   m++;
-   B*=base;
-                            };
+    B/=base;
+    m/=2;
 
-   B/=base;
-   m/=2;
+    //Separando os números a serem multiplicados em duas parcelas contendo seu digitos mais e menos signficativos
+    split_number(&low1, &high1, first, B);
+    split_number(&low2, &high2, second, B); //Operações de precisão múltipla
 
-  //Separando os números a serem multiplicados em duas parcelas contendo seu digitos mais e menos signficativos
-  split_number(&low1, &high1, first, B);
-  split_number(&low2, &high2, second, B);
+   
 
+    //Calculando os produtos intermediários
+    z0 = karatsuba_multiplication(base, low1, low2);
+    z1 = karatsuba_multiplication(base, (low1 + high1), (low2 + high2));
+    z2 = karatsuba_multiplication(base, high1, high2);
 
-  //TESTE USE UM /**/ APÓS O MESMO
-  /*printf128(low1); printf128(high1); printf128(low2); printf128(high2); printf128(m); printf128(B);*/
-
-    
-
-  //Calculando os produtos intermediários
-  z0 = karatsuba_multiplication(base, low1, low2);
-  z1 = karatsuba_multiplication(base, (low1 + high1), (low2 + high2));
-  z2 = karatsuba_multiplication(base, high1, high2);
-
-  //TESTE USE UM /**/ APÓS O MESMO
-  /*printf128(z0); printf128(z1); printf128(z2);*/  
 
   //Resultado
   return ((z2*B*B)+((z1-z2-z0)*B)+z0);
- 
-                                                                                         };
+
+};
 
 
 //******************************************************************************************************************************************
