07420f26b3e5825a1a011650469de72d0d17e61b
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2025-01-07 15:48:19 -0300

    Add files via upload

--- binary_modular_multiplication.h
+++ binary_modular_multiplication.h
@@ -1,80 +1,64 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA A OPERAÇÃO DE MULTIPLICAÇÃO A MENOS DE UMA CONGRUÊNCIA PARA INTEIROS DE 128 BITS
-
-
+// VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA A OPERAÇÃO DE MULTIPLICAÇÃO A MENOS DE UMA CONGRUÊNCIA PARA INTEIROS DE 128 BITS
 
 //****************************************************************************************************************************************
-//CABEÇALHO
+// CABEÇALHO
 #ifndef BINARY_MODULAR_MULTIPLICATION_H
 #define BINARY_MODULAR_MULTIPLICATION_H
-#include"int128.h"
-#include<assert.h>
+#include <assert.h>
+#include "int128.h"
 
 //****************************************************************************************************************************************
-//DECLARAÇÃO DE FUNÇÕES
-__int128_t binary_modular_multiplication(__int128_t,__int128_t, __int128_t);
-
-
-//****************************************************************************************************************************************
-//FUNÇÕES
-__int128_t binary_modular_multiplication(__int128_t a,__int128_t b, __int128_t m){
-//Restrição
-assert(a<m && b<m);
-
-
-//Variáveis locais
-short * bit_array=NULL;
-short bit_size=0;
-__int128_t s=0;
-
-
-//Procedimentos
-//Extraindo os bits da primeira parcela da multiplicação
-while(a>0){
-
-//Testando a paridade do bit
-if((a&1)){
-
-bit_size++;
-bit_array=(short*)realloc(bit_array, bit_size*sizeof(short));
-bit_array[bit_size-1]=1;
-
-         };
-
-if(!(a&1)){
-
-bit_size++;
-bit_array=(short*)realloc(bit_array, bit_size*sizeof(short));
-bit_array[bit_size-1]=0;
-
-          };
-
-//Ajustando variáveis para a próxima iteração
-a>>=1;
-
-          };
-
-
-//Calculando o resultado da multiplicação modular
-for(short i=bit_size; i>=0; --i){
-s<<=1;
-
-if(s>=m)
-s-=m;
-
-if(bit_array[i]==1)
-s+=b;
-
-if(s>=m)
-s-=m;
-                                };
-
-//Limpando o cachê de memória e retornando o resultado da função
-free(bit_array);
-return s;
-
-                                                                                 };
-
+// DECLARAÇÃO DE FUNÇÕES
+__int128_t binary_modular_multiplication(__int128_t, __int128_t, __int128_t);
 
 //****************************************************************************************************************************************
-//FIM DO CABEÇALHO
+// FUNÇÕES
+__int128_t binary_modular_multiplication(__int128_t a, __int128_t b,
+                                         __int128_t m) {
+  // Restrição
+  assert(a < m && b < m);
+
+  // Variáveis locais
+  short* bit_array = NULL;
+  short bit_size = 0;
+  __int128_t s = 0;
+
+  // Procedimentos
+  // Extraindo os bits da primeira parcela da multiplicação
+  while (a > 0) {
+    // Testando a paridade do bit
+    if ((a & 1)) {
+      bit_size++;
+      bit_array = (short*)realloc(bit_array, bit_size * sizeof(short));
+      bit_array[bit_size - 1] = 1;
+    };
+
+    if (!(a & 1)) {
+      bit_size++;
+      bit_array = (short*)realloc(bit_array, bit_size * sizeof(short));
+      bit_array[bit_size - 1] = 0;
+    };
+
+    // Ajustando variáveis para a próxima iteração
+    a >>= 1;
+  };
+
+  // Calculando o resultado da multiplicação modular
+  for (short i = bit_size; i >= 0; --i) {
+    s <<= 1;
+
+    if (s >= m) s -= m;
+
+    if (bit_array[i] == 1) s += b;
+
+    if (s >= m) s -= m;
+  };
+
+  // Limpando o cachê de memória e retornando o resultado da função
+  free(bit_array);
+  return s;
+};
+
+//****************************************************************************************************************************************
+// FIM DO CABEÇALHO
 #endif
--- binary_reciprocation.h
+++ binary_reciprocation.h
@@ -1,123 +1,110 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O CÁLCULO DO RECÍPROCO GENERALIZADO DE UM INTEIRO DE 128 BITS E APLICAÇÕES PARA ARITMÉTICA MODULAR
-//ESSE ALGORITMO DE DIVISÃO E ARITMÉTICA MODULAR É UMA VERSÃO DO ALGORITMO DE BARRET PARA INTEIROS DE 128 BITS.
-
+// VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O CÁLCULO DO RECÍPROCO GENERALIZADO DE
+// UM INTEIRO DE 128 BITS E APLICAÇÕES PARA ARITMÉTICA MODULAR ESSE ALGORITMO DE
+// DIVISÃO E ARITMÉTICA MODULAR É UMA VERSÃO DO ALGORITMO DE BARRET PARA
+// INTEIROS DE 128 BITS.
 
 //****************************************************************************************************************************************
-//CABEÇALHO
+// CABEÇALHO
 #ifndef BINARY_RECIPROCATION_H
 #define BINARY_RECIPROCATION_H
-#include"binary_multiplication.h"
-#include"binary_division.h"
-
+#include "binary_division.h"
+#include "binary_multiplication.h"
 
 //****************************************************************************************************************************************
-//DECLARAÇÕES DE FUNÇÕES
+// DECLARAÇÕES DE FUNÇÕES
 short bit_counter(__int128_t);
 __int128_t binary_reciprocation(__int128_t);
-void binary_modular_division_algorithm(__int128_t*, __int128_t*, __int128_t, __int128_t);
+void binary_modular_division_algorithm(__int128_t*, __int128_t*, __int128_t,
+                                       __int128_t);
 
 //****************************************************************************************************************************************
-//FUNÇÕES 
-//Função que computa o número de bits na string binária de um inteiro de 128 bits
-short bit_counter(__int128_t number){
+// FUNÇÕES
+// Função que computa o número de bits na string binária de um inteiro de 128
+// bits
+short bit_counter(__int128_t number) {
+  // Variáveis locais
+  short counter = 0;
 
-//Variáveis locais
-short counter=0;
+  // Procedimento
+  // Loop principal
+  while (number > 0) {
+    counter++;
+    number >>= 1;
+  };
 
-//Procedimento
-//Loop principal
-while(number>0){
-counter++;
-number>>=1;
-               };
+  // Resultado
+  return counter;
+};
 
-//Resultado
-return counter;
-                                    };
+// Função que calcula o recíproco generalizado para um inteiro de 128 bits
+__int128_t binary_reciprocation(__int128_t n) {
+  // Variáveis locais
+  short bit_size = bit_counter(n);
+  __int128_t r = (2 << (bit_size - 1));
+  __int128_t denominator = r;
+  __int128_t s = r;
+  __int128_t y;
 
+  // Procedimentos
+  // Loop principal
+  do {
+    __int128_t temp = binary_division(binary_multiplication(r, r), denominator);
+    __int128_t temp2 = binary_multiplication(n, temp);
+    __int128_t temp3 = binary_division(temp2, denominator);
 
+    r <<= 1;
+    r -= temp3;
 
-//Função que calcula o recíproco generalizado para um inteiro de 128 bits
-__int128_t binary_reciprocation(__int128_t n){
+    if (s >= r) break;
 
-//Variáveis locais
-short bit_size=bit_counter(n);
-__int128_t r=(2<<(bit_size-1));
-__int128_t denominator=r;
-__int128_t s=r;
-__int128_t y;
+    s = r;
 
+  } while (1);
 
-//Procedimentos
-//Loop principal
-do{
+  // Ajuste fino do resultado
+  y = (2 << (2 * bit_size - 1)) - binary_multiplication(n, r);
 
-__int128_t temp = binary_division(binary_multiplication(r, r), denominator);
-__int128_t temp2 = binary_multiplication(n, temp);
-__int128_t temp3 = binary_division(temp2, denominator);
+  while (y < 0) {
+    r--;
+    y += n;
+  };
 
+  // Resultado
+  return r;
+};
 
-r<<=1;
-r-=temp3;
+// Função que retorna a relação de congruência e limitante inferior do quociente
+// de inteiros de 128 bits usando a redução de Barret
+void binary_modular_division_algorithm(__int128_t* quotient,
+                                       __int128_t* modulus, __int128_t x,
+                                       __int128_t N) {
+  // Variáveis locais
+  __int128_t div = 0;
+  __int128_t R = binary_reciprocation(N);
+  __int128_t d;
+  short s = (2 * (bit_counter(R) - 1));
 
-if(s>=r)
-break;
+  // Procedimentos
+  // Loop principal: redução de Barret
+  while (1) {
+    d = binary_division(binary_multiplication(x, R), (2 << (s - 1)));
+    x = x - binary_multiplication(N, d);
 
-s=r;
+    if (x >= N) {
+      x -= N;
+      d++;
+    };
 
-  }while(1);
+    div += d;
 
+    if (x < N) break;
+  };
 
-//Ajuste fino do resultado
-y=(2<<(2*bit_size-1))-binary_multiplication(n, r);
-
-while(y<0){
-r--;
-y+=n;
-          };
-
-//Resultado
-return r;
-                                             };
-
-
-//Função que retorna a relação de congruência e limitante inferior do quociente de inteiros de 128 bits usando a redução de Barret
-void binary_modular_division_algorithm(__int128_t* quotient, __int128_t* modulus, __int128_t x, __int128_t N){
-
-//Variáveis locais
-__int128_t div=0;
-__int128_t R=binary_reciprocation(N);
-__int128_t d;
-short s=(2*(bit_counter(R)-1));
-
-
-//Procedimentos
-//Loop principal: redução de Barret
-while(1){
-
-d=binary_division(binary_multiplication(x, R), (2<<(s-1)));
-x=x-binary_multiplication(N, d);
-
-if(x>=N){
-x-=N;
-d++;
-
-        };
-
-
-div+=d;
-
-if(x<N)
-break;
-        };
-
-
-//Resultado
-(*quotient)=div;
-(*modulus)=x;
-                                                                                                             };
-
+  // Resultado
+  (*quotient) = div;
+  (*modulus) = x;
+};
 
 //****************************************************************************************************************************************
-//FIM DO HEADER
+// FIM DO HEADER
 #endif
